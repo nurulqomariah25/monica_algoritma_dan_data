bc71d003782647d091bec82438d7d5a4a643ca7a
nurulqomariah25 <nurulqomariah25122005@gmail.com>
2024-09-27 11:11:20 +0700

    Add files via upload

---
+++ biodata monica.py
@@ -0,0 +1,13 @@
+nama_depan = "monica"
+nama_belakang = "monica"
+nama_lengkap = nama_depan +'' + nama_belakang
+usia ='18'
+alamat ='lubuk besar'
+kata_mutiara = "life is simple,but not easy"
+
+print(nama_lengkap, '(' + usia + ')',',',' dari', alamat,',kata mutiara:', kata mutiara)
+
+print('\nTipe dari nama_lengkap:', type(usia)) 
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type (alamat))
+print('tipe dari kata_mutiara:', type(kata_mutiara))
